b03aa2d998c81a0b6546a230593d4c24bf5615d8
A01709150 <A01709150@tec.mx>
2023-11-22 20:25:41 -0600

    feat

---
+++ Examen/Examen/ExamenApp.swift
@@ -0,0 +1,32 @@
+//
+//  ExamenApp.swift
+//  Examen
+//
+//  Created by Diego Perdomo Salcedo on 22/11/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ExamenApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
